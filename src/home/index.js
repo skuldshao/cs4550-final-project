b7b9868ff7d6937e548960276d306796af083131
Ali Schueler <ali.schueler456@gmail.com>
2023-02-28 12:45:54 -0500

    home page

--- src/home/index.js
+++ src/home/index.js
@@ -1,10 +1,18 @@
 import Nav from "../nav";
+import reviews from "./home-reviews.json";
+import {HomeList} from "./home-list";
 
 export const Home = () => {
     return (
         <div>
             <Nav user="user" active="home"/>
-            Home
+            <ul className="list-group">
+            {
+                reviews.map(review => {
+                    return(<HomeList review={review} loggedIn={true}/>);
+                })
+            }
+            </ul>
         </div>
     )
 }
--- src/nav/user-nav.js
+++ src/nav/user-nav.js
@@ -6,17 +6,17 @@ const UserNav = ({active}) => {
         <ul className="nav nav-pills justify-content-between">
             <li className="nav-item">
                 <Link to="/home" className={`nav-link ${active === 'home' ? "active bg-danger" : ""}`}>
-                    <i className={`bi bi-house-door ${active === 'home' ? "" : "bright-red"}`}/>
+                    <i className={`bi bi-house-door ${active === 'home' ? "" : "wd-bright-red"}`}/>
                 </Link>
             </li>
             <li className="nav-item">
                 <Link to="/search" className={`nav-link ${active === 'search' ? "active bg-danger" : ""}`}>
-                    <i className={`bi bi-search ${active === 'search' ? "" : "bright-red"}`}/>
+                    <i className={`bi bi-search ${active === 'search' ? "" : "wd-bright-red"}`}/>
                 </Link>
             </li>
             <li className="nav-item">
                 <Link to="/profile" className={`nav-link ${active === 'profile' ? "active bg-danger" : ""}`}>
-                    <i className={`bi bi-person ${active === 'profile' ? "" : "bright-red"}`}/>
+                    <i className={`bi bi-person ${active === 'profile' ? "" : "wd-bright-red"}`}/>
                 </Link>
             </li>
         </ul>
