466cc289abceb6f6be1a0158bcb05cf4eabcbaa9
Skuld Shao <77602197+skuldshao@users.noreply.github.com>
2023-03-19 18:07:42 -0400

    fix bugs

--- src/login/login.js
+++ src/login/login.js
@@ -9,14 +9,14 @@ function LoginPage() {
 
         <div className='mx-auto' style={{maxWidth:650}}>
 
-            <div className='loginHits text-center fw-bold'>To continue, log in to GoodSounds.</div>
+            <div className='loginHits text-center fw-bold mb-4'>To continue, log in to GoodSounds.</div>
         <div className='loginInputLayout'>
             <label htmlFor="username" className="loginHits">Email Address</label>
             <input type="email" className="inputBox" id="username" placeholder="Enter your email"/>
 
         </div>
             <div className='loginInputLayout'>
-                <label htmlFor="password" className="loginHits">Passwords</label>
+                <label htmlFor="password" className="loginHits">Password</label>
                 <input type="password" className="inputBox" id="password"
                        placeholder="Password"/>
             </div>
@@ -27,11 +27,11 @@ function LoginPage() {
             </div>
 
             <hr/>
-            <div className='loginHits text-center fw-bold'>Don't have a account for GoodSounds?</div>
+            <div className='loginHits text-center fw-bold mb-3'>Don't have a account for GoodSounds?</div>
             <button className='signupButton'>Sign Up for Account</button>
         </div>
 
-        <div className='flex-column align-content-center align-items-center'>
+        <div className='d-flex flex-column align-content-center align-items-center'>
             <img src={music} height={350}/>
         </div>
     </div>
