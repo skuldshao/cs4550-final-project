37d688ffb641b3ed7f615fd2b19568ef02b4040e
Ali Schueler <ali.schueler456@gmail.com>
2023-04-10 09:21:32 -0400

    profiles are linked

--- src/profile/tabs/follow/followItem.js
+++ src/profile/tabs/follow/followItem.js
@@ -10,17 +10,20 @@ function FollowItem( {followItem = {
                          "isFollowing": true
                      }}
 ) {
-    const uid = users.filter(u => u._id === followItem._id)
+    const uid = users.find(u => u._id === followItem._id)._id
+    {console.log(uid)}
     return (
             <div className="col-6 pt-2 pb-2">
                 <div className="d-flex justify-content-between">
-                    <img className="rounded-circle pt-0 align-self-center" width={45} height={45} src={followItem.avatarIcon}/>
-                        <div className="ps-2">
-                            <Link to={`/profile/${uid}`} className="text-white text-decoration-none fs-5 fw-bold ">
-                                {followItem.userName}<br/>
-                                <span className="text-secondary fw-normal"> @{followItem.handle}</span>
-                            </Link>
-                        </div>
+                    <Link to={`/profile/${uid}`}>
+                        <img className="rounded-circle pt-0 align-self-center" width={45} height={45} src={`/images/${followItem.avatarIcon}`}/>
+                    </Link>
+                    <div className="ps-2">
+                        <Link to={`/profile/${uid}`} className="text-white text-decoration-none fs-5 fw-bold ">
+                            {followItem.userName}<br/>
+                            <span className="text-secondary fw-normal"> @{followItem.handle}</span>
+                        </Link>
+                    </div>
                             { followItem.isFollowing ?
                                 <button className="btn btn-outline-danger btn-danger text-black rounded-3 fw-bold rounded-3 ms-auto align-self-center">
                                     FOLLOWING
