7b63d884b1a34d6f68a910d585e848ea30bacc80
Kayla Wang <kayla7843@gmail.com>
2023-04-11 02:17:09 -0400

    updated to search with spotify api

--- src/search/search-bar.js
+++ src/search/search-bar.js
@@ -1,8 +1,119 @@
-import React from "react"
+import React, { useState, useEffect } from "react";
+import {useParams} from "react-router";
+import {Link} from "react-router-dom"
+import {searchTracks} from "../services/spotify-service";
+import {useNavigate} from "react-router";
+
 import "./index.css";
 import SearchResultList from "./search-result-item/search-result-list";
+import SearchResultItem from "./search-result-item/search-result-item";
 
-const SearchBar = () => (
+function SearchBar() {
+    const {query} = useParams();
+    const nav = useNavigate();
+    const [search, setSearch] = useState(query);
+    const [results, setResults] = useState([]);
+
+    const searchSpotify = async () => {
+        const results = await searchTracks(search);
+        //console.log(results);
+        setResults(results);
+        //navigate(`/search?${search}`);
+    };
+    useEffect(() => {
+        if (query) {
+            setSearch(query);
+            searchSpotify().then(r => setResults);
+
+        }
+    }, [query]);
+    return (
+        <>
+            <h1>Song Search</h1>
+            <div className="form-group">
+                <label htmlFor="search-bar">
+                    <span className="visually-hidden">Search</span>
+                </label>
+                <div className="d-flex flex-row position-relative align-items-center justify-ccntent-between mb-2">
+                    <div className="flex-fill">
+                        <input type="text"
+                               id="search-input"
+                               name="search-bar"
+                               placeholder="search"
+                               value={query}
+                               className="form-control rounded-pill ps-5"/>
+                    </div>
+                    <div className="position-absolute">
+                        <button onClick={searchSpotify}
+                                className="btn color-red rounded-circle">
+                            <i className="bi bi-search "/>
+                        </button>
+
+                    </div>
+                </div>
+                <div>
+                    <ul className="list-group">
+                        {results.map((result) => {
+                            return (
+                                <Link to={`/detail/${result.id}`}>
+                                    <br />
+                                    <h2>{result.name.toString()}</h2>
+                                </Link>
+                            );
+                        })}
+                    </ul>
+                </div>
+            </div>
+        </>
+    );
+}
+    /*
+    return (
+        <>
+            <form action="/search" method="get">
+                <label htmlFor="search-bar">
+                    <span className="visually-hidden">Search</span>
+                </label>
+                <div className="d-flex flex-row position-relative align-items-center justify-content-between mb-2">
+                    <div className="position-absolute">
+                        <button type="submit"
+                                onClick={searchSpotify}
+                                className="btn color-red rounded-circle">
+                            <i className="bi bi-search "/>
+                        </button>
+                    </div>
+                    <div className="flex-fill">
+                        <input type="text"
+                               id="test"
+                               name="search"
+                               value={search}
+                               placeholder="Search"
+                               onChange={(e) => setSearch(e.target.value)}
+                               className="form-control rounded-pill ps-5"/>
+
+                    </div>
+                </div>
+            </form>
+            <pre>{JSON.stringify(results, null, 2)}</pre>
+            <ul className="list-group">
+                {
+
+                    results.map((result) => {
+                        return(
+
+                            <li>{result.name}</li>
+                        )
+                    })
+                }
+            </ul>
+        </>
+    )
+}
+
+     */
+
+/*
+const SearchBar1 = () => (
     <>
         <form action="/search" method="get">
             <label htmlFor="search-bar">
@@ -27,5 +138,7 @@ const SearchBar = () => (
         <SearchResultList/>
     </>
 )
+
+ */
 
 export default SearchBar;
