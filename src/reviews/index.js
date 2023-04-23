bf62fa198cb9451c2c4e6991ccebfd1f7e476dab
Ali Schueler <ali.schueler456@gmail.com>
2023-04-23 12:36:43 -0400

    done

--- src/reviews/index.js
+++ src/reviews/index.js
@@ -6,25 +6,30 @@ import reviewArray from './review.json'
 import {useParams} from "react-router";
 
 import {findReviewThunk} from "../services/review-thunk";
-import{useDispatch, useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {findReviewBySongIdThunk} from "../services/review-thunk.js";
 import {profileThunk as adminProfileThunk} from "../services/admin-auth-thunk";
 import {profileThunk as userProfileThunk} from "../services/user-auth-thunk";
 
 
-const ReviewList = ({
-}) => {
+const ReviewList = ({}) => {
     //gets the itemId from url
     const {id} = useParams();
-    const {reviews, loading} = useSelector(state => state.review)
+    const {reviews} = useSelector(state => state.review)
     const dispatch = useDispatch();
     // const [profile, setProfile] = useState({});
     const [loggedIn, setLoggedIn] = useState(false)
+    const [review, setReview] = useState([]);
+    const [loading, setLoading] = useState(true)
     const getProfile = async () => {
         const user = await dispatch(userProfileThunk())
         const loggedInVal = user.type === "userAuth/profile/fulfilled"
-        dispatch(findReviewBySongIdThunk(id))
+        const stuff = await dispatch(findReviewBySongIdThunk(id))
+        setReview(stuff.payload)
+        console.log(stuff.payload)
         setLoggedIn(loggedInVal)
+        setLoading(false)
+
         // if (loggedInVal) {
         //     const userVal = user.payload;
         //     setProfile(userVal);
@@ -35,21 +40,22 @@ const ReviewList = ({
     };
 
     useEffect(() => {
-        dispatch(findReviewBySongIdThunk(id));
+        // dispatch(findReviewBySongIdThunk(id));
+        dispatch(findReviewThunk())
         getProfile();
     }, []);
 
-    return(
-        <ul className = "list-group">
+    return (
+        <ul className="list-group">
             {
                 loading &&
                 <li className="list-group-item">
                     Loading...
                 </li>
             }
-            {
-                reviews.map(review =>
-                    <ReviewItem key={review._id} review={review} loggedIn={loggedIn}/>)
+            {!loading &&
+            reviews.map(review =>
+                <ReviewItem key={review._id} review={review} loggedIn={loggedIn}/>)
             }
         </ul>
     );
--- src/reviews/review-item.js
+++ src/reviews/review-item.js
@@ -5,6 +5,7 @@ import "./rating/index";
 import {profileThunk as userProfileThunk} from "../services/user-auth-thunk";
 import {Link} from "react-router-dom";
 import {findUserByIdThunk, findUserThunk} from "../services/user-thunk";
+import {findReviewThunk} from "../services/review-thunk";
 
 const ReviewItem = (
     {
@@ -30,7 +31,7 @@ const ReviewItem = (
         if (loggedIn && loading) {
             dispatch(userProfileThunk())
             getUserProfile();
-        } else if (loading){
+        } else if (loading) {
             // console.log(profile.handle);
             dispatch(findUserThunk())
             getUserProfile();
@@ -41,31 +42,32 @@ const ReviewItem = (
 
     return (
         <>
-        {!loading && <div className="border text-white" id={review._id}>
-            <div className="flex-row m-3 mb-1">
-                <div className="d-inline-block">
-                    <img src={`/images/profile${reviewer.avatarIcon}.jpeg`}
-                         className="rounded-circle align-self-center"
-                         height="50" width="50"/>
+            {!loading && <div className="border text-white" id={review._id}>
+                <div className="flex-row m-3 mb-1">
+                    <div className="d-inline-block">
+                        <img src={`/images/${reviewer.avatarIcon}`}
+                             className="rounded-circle align-self-center"
+                             height="50" width="50"/>
+                    </div>
+                    <div className="wd-color-white d-inline-block ms-3 mt-3">
+                        <Link to={reviewer._id === profile._id ? '/profile' : `/profile/${reviewer._id}`}
+                              className="text-white text-decoration-none fs-5 fw-bold ">
+                            {reviewer.userName}
+                            <span className="text-secondary fw-normal"> @{reviewer.handle}</span>
+                        </Link><span> gave a rating of </span>
+                        {
+                            [...Array(5).keys()].map(key => key < review.rating ?
+                                <i className="bi bi-star-fill wd-on me-1"/> :
+                                <i className="bi bi-star wd-off me-1"></i>)
+                            //Array(5).map(key => console.log(key))//key < review.rating? <i className="bi bi-star-fill wd-on"/> : <i className="bi bi-star"></i>)
+                        }
+                    </div>
                 </div>
-                <div className="wd-color-white d-inline-block ms-3 mt-3">
-                    <Link to={reviewer._id === profile._id ? '/profile' : `/profile/${reviewer._id}`}
-                          className="text-white text-decoration-none fs-5 fw-bold ">
-                        {reviewer.userName}
-                        <span className="text-secondary fw-normal"> @{reviewer.handle}</span>
-                    </Link><span> gave a rating of </span>
-                    {
-                        [...Array(5).keys()].map(key => key < review.rating ?
-                            <i className="bi bi-star-fill wd-on me-1"/> : <i className="bi bi-star wd-off me-1"></i>)
-                        //Array(5).map(key => console.log(key))//key < review.rating? <i className="bi bi-star-fill wd-on"/> : <i className="bi bi-star"></i>)
-                    }
+                <div className="flex-row ms-5 ps-4">
+                    <p className="ms-2">{review.review}</p>
                 </div>
             </div>
-                <div className="flex-row ms-5 ps-4">
-                <p className="ms-2">{review.review}</p>
-                </div>
-        </div>
-        }
+            }
         </>
     )
 }
